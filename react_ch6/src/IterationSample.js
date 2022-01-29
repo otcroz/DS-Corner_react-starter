bc28bf36239f4ae84bf34e66413c5d3f2664df7a
yxxyn20 <dbtndus0527@gmail.com>
2022-01-29 21:27:19 +0900

    초기 상태 설정 및 데이터 추가 기능 구현하기

--- react_ch6/src/IterationSample.js
+++ react_ch6/src/IterationSample.js
@@ -1,8 +1,37 @@
-const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+import { useState } from "react";
 
-  return <ul>{nameList}</ul>;
+const IterationSample = () => {
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    //버튼 클릭하면 데이터 추가
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
